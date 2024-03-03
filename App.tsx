624398bf796e3225c9a29f939761f788f47967ee
JackNi49 <Jackni0241@gmail.com>
2024-03-02 21:37:33 -0500

    remove shadcn due not really supported by React Native

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Testing from 'src/components/testing';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Testing/>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
