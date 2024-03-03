8b8b8076d775e1fa3e1e040847a7f1298b289cb1
JackNi49 <Jackni0241@gmail.com>
2024-03-02 20:06:00 -0500

    testing lower case

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { buttonVariants } from "@/components/ui/Button"
+import { Button } from "@/components/ui/button"
 import { Link } from 'lucide-react';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <Link className={buttonVariants({ variant: "outline" })}>Click here</Link>
+    <Link>
+      <Button>HI</Button>
+    </Link>
 
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
